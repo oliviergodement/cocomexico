97f7a7b38b5268bf2dce2e9098cacb8b700fd424
Olivier Godement <olivier.godement@gmail.com>
2014-05-16 17:55:53 +0200

    seeding cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Ingredient.create(name: "Gin", photo_url: "http://i2.cdscdn.com/pdt2/8/8/0/1/700x700/mhud00880/rw/gin-gordon-s-1-litre.jpg")
-# Ingredient.create(name: "Sugar", photo_url: 'http://dietsindetails.com/userfiles/sugar(1).jpg')
-# Ingredient.create(name: "Lemon", photo_url: "http://www.vapertrail.co.uk/ekmps/shops/elsabenoit/images/new-vaper-trails-lemon-e-liquid-911-p.jpg")
-# Ingredient.create(name: "Carbonated Water", photo_url: "http://www.dnyland.com/upload/category/a22812_sparkling_water.jpg")
+Ingredient.create(name: "Gin", photo_url: "http://i2.cdscdn.com/pdt2/8/8/0/1/700x700/mhud00880/rw/gin-gordon-s-1-litre.jpg")
+Ingredient.create(name: "Sugar", photo_url: 'http://dietsindetails.com/userfiles/sugar(1).jpg')
+Ingredient.create(name: "Lemon", photo_url: "http://www.vapertrail.co.uk/ekmps/shops/elsabenoit/images/new-vaper-trails-lemon-e-liquid-911-p.jpg")
+Ingredient.create(name: "Carbonated Water", photo_url: "http://www.dnyland.com/upload/category/a22812_sparkling_water.jpg")
 
-# Cocktail.create(name:"Gin Fizz", like: 0, photo_url: "http://img3.wikia.nocookie.net/__cb20110602172135/bienrecevoirchezsoi/fr/images/e/e8/Ginfizz.jpg")
+Cocktail.create(name:"Gin Fizz", like: 0, photo_url: "http://img3.wikia.nocookie.net/__cb20110602172135/bienrecevoirchezsoi/fr/images/e/e8/Ginfizz.jpg")
 
-# # Dosage.create(cocktail_id: 2, ingredient_id:2, quantity: "1/4")
-# Dosage.create(cocktail_id: 2, ingredient_id:3, quantity: "2")
-# Dosage.create(cocktail_id: 2, ingredient_id:4, quantity: "1/2")
-# Dosage.create(cocktail_id: 2, ingredient_id:5, quantity: "1/2")
+Dosage.create(cocktail_id: 1, ingredient_id:1, quantity: "1")
+Dosage.create(cocktail_id: 1, ingredient_id:2, quantity: "2")
+Dosage.create(cocktail_id: 1, ingredient_id:3, quantity: "1")
+Dosage.create(cocktail_id: 1, ingredient_id:4, quantity: "1")
